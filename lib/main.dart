798be0a5e476f2ee50ee24cdd7ef88b2f44858e9
Martypose <martinpose@hotmail.com>
2022-11-29 01:44:02 +0100

    Avanzando

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:collection';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -41,7 +43,62 @@ class Person {
     String? uuid,
   }) : uuid = uuid ?? const Uuid().v4();
 
+
+  Person updated([String? name, int? age]) => Person(
+      name: name ?? this.name,
+      age: age ?? this.age,
+      uuid: uuid,
+    );
+
+  String get displayName => '$name ($age yeas old)';
+
+  @override
+  bool operator ==(covariant Person other) => uuid == other.uuid;
+
+  @override
+  int get hashCode => uuid.hashCode;
+
+  @override
+  String toString() => 'Person(name: $name, age: $age, uuid: $uuid)';
+
 }
+
+
+final peopleProvider = ChangeNotifierProvider(
+  (_) => DataModel(),
+);
+
+class DataModel extends ChangeNotifier {
+  final List<Person> _people = [];
+
+  int get count => _people.length;
+
+  UnmodifiableListView<Person> get people => UnmodifiableListView(_people);
+
+  void add(Person person) {
+    _people.add(person);
+    notifyListeners();
+  }
+
+  void remove(Person person) {
+    _people.remove(person);
+    notifyListeners();
+  }
+
+void update(Person updatedPerson){
+  final index = _people.indexOf(updatedPerson);
+  final oldPerson = _people[index];
+  if(oldPerson.name != updatedPerson.name || oldPerson.age != updatedPerson.age){
+    _people[index] = oldPerson.updated(updatedPerson.name, updatedPerson.age);
+    notifyListeners();
+  }
+  notifyListeners();
+  }
+
+
+
+}
+
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
@@ -57,3 +114,5 @@ class HomePage extends ConsumerWidget {
     );
   }
 }
+
+final nameController = TextEditingController();
